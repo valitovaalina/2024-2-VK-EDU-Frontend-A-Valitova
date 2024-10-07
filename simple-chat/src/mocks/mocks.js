f6d19a79f253a7eeb18db0bf8ebf760ab07f97de
Valitova Alina <valitovaalina1501@gmail.com>
2024-10-08 01:40:17 +0500

    поправила пути аватарки

--- simple-chat/src/mocks/mocks.js
+++ simple-chat/src/mocks/mocks.js
@@ -2,7 +2,7 @@ export const chatsMocks = [
     {
         id: '1',
         name: 'Оля',
-        avatar: '../images/avatar_1.jpg',
+        avatar: './images/avatar_1.jpg',
         messages: [
             {text: 'kkkkkkkkkkkkkkkkk', date: new Date('2024-09-21T22:13:10-04:12')},
             {text: 'bbbbbbbbbbbbbbbbbb', date: new Date('2024-10-21T22:14:30-04:12')},
@@ -13,7 +13,7 @@ export const chatsMocks = [
     {
         id: '2',
         name: 'Дима',
-        avatar: '../images/avatar_1.jpg',
+        avatar: './images/avatar_1.jpg',
         messages: [
             {text: 'cccccccccccccccc', date: new Date('2024-04-21T22:15:45-04:12')},
         ],
@@ -22,7 +22,7 @@ export const chatsMocks = [
     {
         id: '3',
         name: 'Ваня',
-        avatar: '../images/avatar_1.jpg',
+        avatar: './images/avatar_1.jpg',
         messages: [
             {text: 'aaaaaaaaaaaaaaaaaa', date: new Date('2024-09-21T22:13:10-04:12')},
             {text: 'bbbbbbbbbbbbbbbbbb', date: new Date('2024-10-21T22:14:30-04:12')},
@@ -33,7 +33,7 @@ export const chatsMocks = [
     {
         id: '4',
         name: 'Лиза',
-        avatar: '../images/avatar_1.jpg',
+        avatar: './images/avatar_1.jpg',
         messages: [
             {text: 'aaaaaaaaaaaaaaaaaa', date: new Date('2024-09-21T22:13:10-04:12')},
             {text: 'bbbbbbbbbbbbbbbbbb', date: new Date('2024-10-21T22:14:30-04:12')},
@@ -44,7 +44,7 @@ export const chatsMocks = [
     {
         id: '5',
         name: 'Катя',
-        avatar: '../images/avatar_1.jpg',
+        avatar: './images/avatar_1.jpg',
         messages: [
             {text: 'aaaaaaaaaaaaaaaaaa', date: new Date('2024-09-21T22:13:10-04:12')},
             {text: 'bbbbbbbbbbbbbbbbbb', date: new Date('2024-10-21T22:14:30-04:12')},
@@ -55,7 +55,7 @@ export const chatsMocks = [
     {
         id: '6',
         name: 'Арина',
-        avatar: '../images/avatar_1.jpg',
+        avatar: './images/avatar_1.jpg',
         messages: [
             {text: 'cccccccccccccccc', date: new Date('2024-04-21T22:15:45-04:12')},
         ],
@@ -64,7 +64,7 @@ export const chatsMocks = [
     {
         id: '7',
         name: 'Максим',
-        avatar: '../images/avatar_1.jpg',
+        avatar: './images/avatar_1.jpg',
         messages: [
             {text: 'aaaaaaaaaaaaaaaaaa', date: new Date('2024-09-21T22:13:10-04:12')},
             {text: 'bbbbbbbbbbbbbbbbbb', date: new Date('2024-10-21T22:14:30-04:12')},
@@ -75,7 +75,7 @@ export const chatsMocks = [
     {
         id: '8',
         name: 'Вова',
-        avatar: '../images/avatar_1.jpg',
+        avatar: './images/avatar_1.jpg',
         messages: [
             {text: 'aaaaaaaaaaaaaaaaaa', date: new Date('2024-09-21T22:13:10-04:12')},
             {text: 'bbbbbbbbbbbbbbbbbb', date: new Date('2024-10-21T22:14:30-04:12')},

9ee0e9313731ee895b5317670c86df36e6e97c6c
Valitova Alina <valitovaalina1501@gmail.com>
2024-10-09 11:58:36 +0500

    добавила кнопку создания нового чата

--- simple-chat/src/pages/chats-page.js
+++ simple-chat/src/pages/chats-page.js
@@ -1,6 +1,7 @@
 import {createChatsPageHeader} from '../components/chats-page-header/chats-page-header';
 import {createChatsPageItem} from '../components/chats-page-item/chats-page-item';
 import {createChatsPageItemsList} from '../components/chats-page-items-list/chats-page-items-list'
+import {createNewChatButton} from '../components/new-chat-button/new-chat-button';
 import {createElement} from '../helpers/createElement';
 import {chatsMocks} from '../mocks/mocks';
 
@@ -8,8 +9,9 @@ export const createChatsPage = ({userId}) => {
     const container = createElement('div', 'chats-page-container');
     const header = createChatsPageHeader();
     const chats = createChatsPageItemsList();
+    const newChatButton = createNewChatButton();
 
-    container.append(header, chats);
+    container.append(header, chats, newChatButton);
 
     return container;
 };

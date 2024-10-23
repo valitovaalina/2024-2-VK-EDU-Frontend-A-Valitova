8d8888d816836112d31b919c6ab4dbee7bca08b0
Valitova Alina <valitovaalina1501@gmail.com>
2024-10-24 04:14:47 +0500

    все еще пытаюсь

--- react-chat/vite.config.ts
+++ react-chat/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://valitovaalina.github.io/2024-2-VK-EDU-Frontend-A-Valitova/'
+  base: '2024-2-VK-EDU-Frontend-A-Valitova'
 })

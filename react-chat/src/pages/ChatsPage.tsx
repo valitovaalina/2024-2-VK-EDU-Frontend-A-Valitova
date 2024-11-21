eb484af5080a914ec4c7cdd9772b4e6792b7bab6
Valitova Alina <valitovaalina1501@gmail.com>
2024-11-22 00:36:17 +0500

    связала страницу профиля с редаксом

--- react-chat/src/pages/ChatsPage.tsx
+++ react-chat/src/pages/ChatsPage.tsx
@@ -8,14 +8,12 @@ import {NewChatButton} from '../components/NewChatButton/NewChatButton';
 import {ChatsApi} from '../api/chats';
 import {AppApiRoute} from '../consts/AppRoute';
 import {useAppDispatch, useAppSelector} from '../hooks/useStore';
-import {getUserChats} from '../store/ChatsReducer/ChatsSelectors';
 import {getUsers} from '../store/UserReducer/UserSelectors';
 import {fetchChats, fetchUsers} from '../store/apiActions';
 
 export const ChatsPage: FC = () => {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
-    const chats = useAppSelector(getUserChats);
     const users = useAppSelector(getUsers);
     const [isChatAdded, setIsChatAdded] = useState(false);
     const chatsApi = new ChatsApi();
@@ -39,19 +37,19 @@ export const ChatsPage: FC = () => {
         });
 
         return () => setIsChatAdded(false);
-    }, [isChatAdded]);
+    }, [isChatAdded, dispatch]);
 
     useEffect(() => {
         dispatch(fetchUsers()).catch((err: AxiosError) => {
             navigate(AppApiRoute.Login);
             alert(err);
         });
-    }, []);
+    }, [dispatch]);
 
     return (
         <div>
             <ChatsPageHeader />
-            <ChatsPageItemsList chats={chats} />
+            <ChatsPageItemsList />
             <NewChatButton onClick={addChat} />
         </div>
     );

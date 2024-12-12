b0b7825f00a57ea6e9659ede0ba9e5cd1e8e553b
Valitova Alina <valitovaalina1501@gmail.com>
2024-12-13 01:10:04 +0500

    поправила ошибку линтера

--- react-chat/src/pages/ChatPage.tsx
+++ react-chat/src/pages/ChatPage.tsx
@@ -17,12 +17,6 @@ export const ChatPage: FC = () => {
     const [inputValue, setInputValue] = useState<string>('');
     const messagesRef = useRef<HTMLUListElement>() as RefObject<HTMLUListElement>;
 
-    
-    if (!id) {
-        navigate(AppApiRoute.Chats);
-        return null;
-    }
-
     useEffect(() => {
         if (!id)
             return;
@@ -45,6 +39,11 @@ export const ChatPage: FC = () => {
 
         return () => clearInterval(timerId);
     }, [dispatch, id, navigate]);
+
+    if (!id) {
+        navigate(AppApiRoute.Chats);
+        return null;
+    }
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
